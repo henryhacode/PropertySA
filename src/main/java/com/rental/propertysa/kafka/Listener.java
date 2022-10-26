68a5c29e4c7692264f9c9142d44e12cb16b8bd9f
Thiep Ha - Henry <thiep.ha@miu.edu>
2022-10-26 14:29:37 -0500

    correct kafka listener

--- src/main/java/com/rental/propertysa/kafka/Listener.java
+++ src/main/java/com/rental/propertysa/kafka/Listener.java
@@ -5,13 +5,15 @@ import com.rental.propertysa.repository.PropertyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class Listener {
     private final PropertyRepository propertyRepository;
 
-    @KafkaListener(id = "reservation-listener", topics = "ReservationDoneEvent")
+    @KafkaListener(id = "reservation-listener", topics = "${kafka.reservationTopic}")
     public void listen(ReservationChange msg) {
         log.info("Consumer received msg " + msg);
         if (msg == null) return;
